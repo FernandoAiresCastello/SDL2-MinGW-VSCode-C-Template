273e0ccd7936f5c5d81e7fb5481a35fd0c676b02
Fernando <fernandoairescastello@gmail.com>
2020-01-27 18:01:00 -0300

    Added -mwindows compiler option to hide console window

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ if exist "template.exe" (
     del "template.exe"
 )
 
-gcc -o template.exe template.c -lmingw32 -lSDL2main -lSDL2
+gcc -o template.exe template.c -mwindows -lmingw32 -lSDL2main -lSDL2
 
 if not errorlevel 1 (
     "template.exe"
